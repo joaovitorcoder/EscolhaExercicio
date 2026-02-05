fa832e0bd043316fc292b9c9a5f9b25b4897e3ad
João Vitor <olhierturcojoaovitor@gmail.com>
2026-02-05 13:33:23 -0300

    att

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@ let rotacaoAtual = 0
 function girar(){
     const roleta = document.getElementById("roleta");
     
-    const giro = Math.floor(Math.random() * 360) + 1440;
+    const giro = Math.floor(Math.random() * 360) + 4600;
     rotacaoAtual += giro;
     roleta.style.transform = `rotate(${rotacaoAtual}deg)`;
 }
